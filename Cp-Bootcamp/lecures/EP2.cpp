dd323fd54298c2e18412cfaaca064964cfa0b341
Bhawesh Choudhary <bhaweshchoudhary100@gmail.com>
2024-12-04 23:55:49 +0530

    first commit

---
+++ Cp-Bootcamp/lecures/EP2.cpp
@@ -0,0 +1,129 @@
+// #include <iostream> 
+// Header Files, Which contains all the c
+
+
+
+
+#include<bits/stdc++.h> 
+using namespace std;
+
+
+// main is a function
+
+int main()
+{
+	#ifndef ONELINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt","w", stdout);
+	#endif
+
+
+
+
+//  Intro 
+	// int a = 2;
+	// int b = 3;
+	// int sum = 2+3;
+
+
+
+
+
+
+// Syntax ++++++++++++++++++++++++++++++++
+
+	// cout << "HellOooo"<< ;
+	// cout << sqrt(16);
+
+
+
+
+
+
+
+
+
+//  Variables ++++++++++++++
+
+	// int a = 2;
+	// int b = 3;
+	// int sum = a+b;
+
+
+
+
+
+
+// Data Types +++++++++++++++
+
+	// char : stores characters ABC..
+	// int : stores integers
+	// float : stores decimals numbers
+	// double : stores decimals numbers
+
+	// char c = 'b';
+	// int a = 2;
+	// double d = 4.34;
+	// bool l = 123; // bool always takes TRUE, It'll be FALSE only for 0.
+	// long long lin = 12342; // int < long < long long
+
+	// cout << c << "  " << a << " "<< d << " " << l << endl;
+	
+
+
+// Arithmetic Operators ++++++++++++++++++++++++++
+
+	// * : Multiply
+	// % : Modulo, gives remainder
+	// ++ : First Increment, then value used.
+	// a++ : val a used then, increment
+	// ++a : val a incremented then, value will be used
+	// a-- : Val a used, then incermented.
+
+
+	// % :
+	// cout << 25%7 << endl;
+
+
+	// ++ :
+
+	// a++
+	int a = 9;
+	// a = a + 1; // or a++
+	// cout << a << endl;
+	// cout << a++ << endl;
+	// cout << ++a << endl;
+	// cout << a-- << endl;
+	// cout << --a << endl;
+
+
+
+	// it'll 1st use the value of a and then increment it.
+	// int a = 5;
+	// a++
+
+
+
+// Relationnal Operators
+	// == , != , >,<, >=, <=
+
+
+
+// Characters 
+	// char c = 'a';
+	// ASCII values are there for each characters.
+	// to checkout the value of chars to ASCII, 
+	// use : count << (int)c;
+	// i.e it'll use the ASCII value of chars.
+
+	// endl or "\n" : to print new lines
+	// Chars used, single ''
+	// char only takes a single value at a time.
+	
+
+
+
+
+
+}
+
